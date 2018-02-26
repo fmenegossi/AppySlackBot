ff5e12aa80d6fe7a1584b004361a7faa0fd56dd7
renato niola <niolarenato01@gmail.com>
2018-02-26 17:57:29 +0100

    fetched devlopment

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const http = require('http')
+
+const getApis= require('./db/seed')
+
+
+const port = process.env.PORT || 3030
+
+const app = express()
+const server = http.Server(app)
+
+
+const getData = function() {
+  getApis.getApis(function(data){
+      console.log('data : ', data.d)
+  })
+}
+
+setInterval( getData ,3000)
+
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found')
+    err.status = 404
+    next(err)
+  })
+
+  .use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send({
+      message: err.message,
+      error: app.get('env') === 'development' ? err : {}
+    })
+  })
+
+server.listen(port)
