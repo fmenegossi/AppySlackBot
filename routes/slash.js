1d689269de8b25e9f2ae00d8a9100da96d7a101a
Felipe Menegossi <felipe.menegossi@gmail.com>
2018-02-27 11:12:12 +0100

    Supposing that it may work like that

--- routes/slash.js
+++ routes/slash.js
@@ -1,9 +1,21 @@
 const router = require('express').Router()
 
-router.post('/slashcom/:apiname', (req, res) => {
+router.post('/slashcom/:apiname', (req, res, next) => {
   const apiName = req.params.apiname
 
-  getApis( )
+  getApis()
+    .then(() => {
+      Api.find({name: apiName})
+        .then((api) => {
+          if(!api){
+            err = new Error(`API ${apiName} not found`)
+            err.status = 404
+            next(err)
+          }
+
+          res.json(api)
+        })
+    })
 })
 
 module.exports = router
