ed1036e83af7b8783df92ecb71f2a92c165fb554
IrynaOvdii <irina.ovdij@gmail.com>
2018-02-28 11:43:02 +0100

    Find one API

--- routes/slash.js
+++ routes/slash.js
@@ -1,18 +1,36 @@
+const {Api } = require('../models')
 const router = require('express').Router()
 
+// router.post('/api/getstatus', (req, res, next) => {
+//
+//     console.log('data : ' , req.body)
+//
+//     res.send(
+//     {
+//         "text": req.body.text,
+//         "attachments": [
+//             {
+//                 "text":"Hello! It's status from AppyBot!"
+//             }
+//         ]
+//     })
+
+
+
 router.post('/api/getstatus', (req, res, next) => {
-
-    console.log('data : ' , req.body)
-
-    res.send(
-    {
-        "text": req.body.text,
-        "attachments": [
-            {
-                "text":"Hello! It's status from AppyBot!"
-            }
-        ]
+    console.log('data : ' , req.body.text)
+    Api.findOne({'name':req.body.text},function(err,foundApi){
+      if(err){throw err}
+      if(foundApi == null){
+        console.log('there is no Api with this name')
+        return null
+        }
+      }).then((foundApi) => {
+      res.send(foundApi)
     })
+    .catch((error) => next(error))
+})
+module.exports = router
 
   // const apiName = req.params.apiname
   //
