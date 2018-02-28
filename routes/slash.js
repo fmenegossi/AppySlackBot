eef0ac453e5d414daa77e2cdfe82744178c34aab
doperwt <r.f.denouden@gmail.com>
2018-02-28 14:52:47 +0100

    refactoring a little

--- routes/slash.js
+++ routes/slash.js
@@ -9,6 +9,7 @@ const password = process.env.SAP_PASSWORD
 const tokenUrl = config.TOKEN_URL
 const dataUrl = config.DATA_URL
 const {Api } = require('../models')
+const createApi = require('../lib/checkData').createApi
 
 router.post('/api/getstatus', (req, res, next) => {
   getApis(username,password,tokenUrl,dataUrl)
@@ -16,14 +17,20 @@ router.post('/api/getstatus', (req, res, next) => {
     let apis = data.d.results.map(api =>  {
       return reformatData(api)
     })
+
     if(typeof req.body.text !== 'undefined'){
       let text = req.body.text.trim().toLowerCase()
       let api = apis.filter(api => api.name.toLowerCase() === text)[0]
+
       if(!!api){
         let update = 'Api :'+api.name+'\n was last changed by:'+api.changed_by+'\n on:'+api.changed_at
         Api.findOne({'name':api.name},function(err,foundApi){
-          updateApi(api,foundApi)})
-          res.send({text:update})
+          if(!!foundApi){
+          updateApi(api,foundApi)
+
+        }else { createApi(api) } } )
+        res.send({text:update})
+
         } else {
           let list = apis.map(api =>  api.name)
           let update = 'Api not found we have the following Apis:'+list.join(' ')
@@ -31,11 +38,11 @@ router.post('/api/getstatus', (req, res, next) => {
         }
       } else {
         let list = apis.map(api =>  api.name)
-
         let update = 'No text entered list of Apis:'+list.join(' ')
         res.send({text:update})
       }
     })
+    .catch((err)=>{console.log(err)})
   })
 
   module.exports = router
