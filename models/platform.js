d891c1440a9c8f0e8d219bc06cad85ef5d67e600
Felipe Menegossi <felipe.menegossi@gmail.com>
2018-03-01 15:05:33 +0100

    Create platform route. Change API schema.

--- models/platform.js
+++ models/platform.js
@@ -2,7 +2,7 @@ const mongoose = require('../config/database')
 const { Schema } = mongoose
 
 const platformSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   url: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true }
