4c6cfaca6a4ff6cc22a7a2683494922424507dcb
renato niola <niolarenato01@gmail.com>
2018-03-01 16:10:26 +0100

    fix colors mistakes

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const TOKEN_URL = 'https://apiportalu34f5b50f-p1942719152trial.hanatrial.ondeman
 const DATA_URL = 'https://apiportalu34f5b50f-p1942719152trial.hanatrial.ondemand.com/apiportal/api/1.0/Management.svc/APIProxies?$select=name,life_cycle,state&$format=json'
 const SLACKBOT_URL = 'https://hooks.slack.com/services/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'
 
-const WORNING_COLOR = "#ad0505"
+const WARNING_COLOR = "#ad0505"
 const SUCCESS_COLOR = "#005D00"
 
 const INTERVAL = 30000
@@ -13,5 +13,5 @@ module.exports = {
        SLACKBOT_URL,
        INTERVAL,
        SUCCESS_COLOR,
-       WORNING_COLOR
+       WARNING_COLOR
      }
