6aeabc944f5a4cd61e4fa8dd8632f4ebd2baa7a6
folkert89 <folkertburghout@hotmail.com>
2018-03-05 16:47:33 +0100

    refactored the platform route

--- routes/platform.js
+++ routes/platform.js
@@ -1,54 +1,88 @@
 const router = require('express').Router()
 const { Platform } = require('../models')
 const { properPlatformFormat } = require('../lib/messages')
+
+const isJson = (requestBody) => {
+  try {
+    JSON.parse(requestBody)
+    return true
+  } catch (err) {
+    return false
+  }
+}
+
 router
   .post('/api/platforms', (req, res, next) => {
-      console.log(typeof req.body.text)
 
-    // const test = {"blaat":"bla"}
-    // const data = JSON.parse(test)
-    const requestBody = req.body.text
-
-    if((!requestBody) ||
-       (requestBody.slice(-1) !== '}' && requestBody.slice(0,1) !== '{')) {
-      res.send(properPlatformFormat())
-    } else {
-      const data = JSON.parse(requestBody)
-      
-      if (!data.name || !data.url || !data.username || !data.password){
+    function checkBody(text) {
+      if ((!text) ||
+        (text.slice(-1) !== '}' && text.slice(0, 1) !== '{') || !isJson(text)) {
         res.send(properPlatformFormat())
       } else {
-        Platform.findOne({ name: data.name })
-        .then((platform) => {
-          if(!platform) {
-            const newPlatform = {
-              name: data.name,
-              url: data.url,
-              username: data.username,
-              password: data.password
-            }
-
-            Platform.create(newPlatform, (error, platform) => {
-              if(error) { next(error) }
-              res.send(platform)
-            })
-          } else {
-            const platformUpdates = {
-              url: data.url,
-              username: data.username,
-              password: data.password
-            }
-
-            const patchedPlatform = { ...platform, ...platformUpdates }
-
-            Platform.findByIdAndUpdate(platform._id, { patchedPlatform }, { new: true })
-              .then((platform) => {
-                res.send(platform)
-              })
-          }
-        })
+        const data = JSON.parse(text)
+        checkFormat(data)
       }
     }
+
+    function checkFormat(data) {
+      if (!data.name || !data.url || !data.username || !data.password) {
+        res.send(properPlatformFormat())
+      } else {
+        findPlatform(data)
+      }
+    }
+
+    function findPlatform(data) {
+      Platform.findOne({
+          name: data.name
+        })
+        .then((platform) => {
+          if (!platform) {
+            createPlatform(data)
+          } else {
+            updatePlatform(platform, data)
+          }
+        })
+    }
+
+    function createPlatform(data) {
+      const newPlatform = {
+        name: data.name,
+        url: data.url,
+        username: data.username,
+        password: data.password
+      }
+
+      Platform.create(newPlatform, (error, platform) => {
+        if (error) {
+          next(error)
+        }
+        res.send(platform)
+      })
+    }
+
+    function updatePlatform(platform, data) {
+      const platformUpdates = {
+        url: data.url,
+        username: data.username,
+        password: data.password
+      }
+
+      const patchedPlatform = { ...platform,
+        ...platformUpdates
+      }
+
+      Platform.findByIdAndUpdate(platform._id, {
+          patchedPlatform
+        }, {
+          new: true
+        })
+        .then((platform) => {
+          res.send(platform)
+        })
+    }
+
+    checkBody(req.body.text)
   })
 
 module.exports = router
