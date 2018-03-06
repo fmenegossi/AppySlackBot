aabb46ed8be793fffb3e8be80f8d17c7ab6bc671
doperwt <r.f.denouden@gmail.com>
2018-03-06 15:55:07 +0100

    added new route

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ const TOKEN_URL = 'https://apiportalu34f5b50f-p1942719152trial.hanatrial.ondeman
 const DATA_URL = 'https://apiportalu34f5b50f-p1942719152trial.hanatrial.ondemand.com/apiportal/api/1.0/Management.svc/APIProxies?$select=name,life_cycle,state&$format=json'
 const SLACKBOT_URL = 'https://hooks.slack.com/services/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'
 
+
 const SLASH_TOKEN = 'xmwZYiyHJdzeJvbSaqjqpdNF'
 const INTERVAL = 30000
 const WARNING_COLOR = "#ad0505"
