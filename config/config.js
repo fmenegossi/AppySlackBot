560b7111795dd7a879bffa092b2234dc174d83a5
doperwt <r.f.denouden@gmail.com>
2018-03-06 10:57:08 +0100

    interval changed

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const DATA_URL = 'https://apiportalu34f5b50f-p1942719152trial.hanatrial.ondemand
 const SLACKBOT_URL = 'https://hooks.slack.com/services/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'
 
 const SLASH_TOKEN = 'xmwZYiyHJdzeJvbSaqjqpdNF'
-const INTERVAL = 300000
+const INTERVAL = 30000
 const WARNING_COLOR = "#ad0505"
 const SUCCESS_COLOR = "#005D00"
 
