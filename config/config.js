23b9897b4f10eab1eae692f893dc4813d32379a5
doperwt <r.f.denouden@gmail.com>
2018-03-06 13:34:11 +0100

    refactoring, readability changes

--- config/config.js
+++ config/config.js
@@ -1,5 +1,3 @@
-const TOKEN_URL = 'https://apiportalu34f5b50f-p1942719152trial.hanatrial.ondemand.com/apiportal/api/1.0/Management.svc'
-const DATA_URL = 'https://apiportalu34f5b50f-p1942719152trial.hanatrial.ondemand.com/apiportal/api/1.0/Management.svc/APIProxies?$select=name,life_cycle,state&$format=json'
 const SLACKBOT_URL = 'https://hooks.slack.com/services/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'
 
 const SLASH_TOKEN = 'xmwZYiyHJdzeJvbSaqjqpdNF'
@@ -8,8 +6,6 @@ const WARNING_COLOR = "#ad0505"
 const SUCCESS_COLOR = "#005D00"
 
 module.exports = {
-       TOKEN_URL ,
-       DATA_URL,
        SLACKBOT_URL,
        INTERVAL,
        SLASH_TOKEN,
