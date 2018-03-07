26041ae75f29c148e90f85dcbc4ae2261a845a6a
doperwt <r.f.denouden@gmail.com>
2018-03-07 17:41:26 +0100

    using webhookers

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const TOKEN_URL = 'https://apiportalu34f5b50f-p1942719152trial.hanatrial.ondemand.com/apiportal/api/1.0/Management.svc'
 const DATA_URL = 'https://apiportalu34f5b50f-p1942719152trial.hanatrial.ondemand.com/apiportal/api/1.0/Management.svc/APIProxies?$select=name,life_cycle,state&$format=json'
-const SLACKBOT_URL = 'https://hooks.slack.com/services/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'
+const SLACKBOT_URL = 'https://hooks.slack.com/services/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'
 
 const SLASH_TOKEN = 'ZXl4QW0LJ3k9pc9pkSqIS5Ds'
 const INTERVAL = 30000
