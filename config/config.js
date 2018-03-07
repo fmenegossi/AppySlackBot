a0cc86a80683eccb7e152653d5785de7760295bc
Felipe Menegossi <felipe.menegossi@gmail.com>
2018-03-07 17:50:15 +0100

    Little refactoring :)

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const TOKEN_URL = 'https://apiportalu34f5b50f-p1942719152trial.hanatrial.ondemand.com/apiportal/api/1.0/Management.svc'
 const DATA_URL = 'https://apiportalu34f5b50f-p1942719152trial.hanatrial.ondemand.com/apiportal/api/1.0/Management.svc/APIProxies?$select=name,life_cycle,state&$format=json'
-const SLACKBOT_URL = 'https://hooks.slack.com/services/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'
+const SLACKBOT_URL = 'https://hooks.slack.com/services/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'
 
 const SLASH_TOKEN = 'xmwZYiyHJdzeJvbSaqjqpdNF'
 const INTERVAL = 30000
@@ -8,11 +8,11 @@ const WARNING_COLOR = "#ad0505"
 const SUCCESS_COLOR = "#005D00"
 
 module.exports = {
-       TOKEN_URL ,
-       DATA_URL,
-       SLACKBOT_URL,
-       INTERVAL,
-       SLASH_TOKEN,
-       SUCCESS_COLOR,
-       WARNING_COLOR
-     }
+  TOKEN_URL ,
+  DATA_URL,
+  SLACKBOT_URL,
+  INTERVAL,
+  SLASH_TOKEN,
+  SUCCESS_COLOR,
+  WARNING_COLOR
+}
